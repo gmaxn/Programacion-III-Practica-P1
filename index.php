3bee20087f955d49429a8054c482a2404257b220
gmaxn <maxi.ng@hotmail.com>
2020-05-01 12:03:29 -0300

    handling incoming requests

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+date_default_timezone_set('America/Argentina/Buenos_Aires');
+
+$path_info = $_SERVER['PATH_INFO'] ?? '';
+
+$resource = '/' . (explode('/', $path_info)[1] ?? '');
+
+
+switch ($resource) {
+
+    case '/personas':
+
+    break;
+
+    case '/alumnos':
+
+    break;
+
+    default:
+        echo 'Requested URL:' . $path_info;
+        echo $resource . ' is not a valid resource';
+    break;
+}
